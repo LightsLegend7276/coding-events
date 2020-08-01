32dca00388a3ab8a5191176cef3a181a88fb7ead
Denis Duarte <duarte.denis11@gmail.com>
2020-08-01 15:42:09 -0400

    coding-events practice

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="event")
+public class EventController {
+
+
+
+
+}
