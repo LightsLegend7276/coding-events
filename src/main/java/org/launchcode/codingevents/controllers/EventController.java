deb285e751ee683b5a64c6ecddc7898433ead76a
Denis Duarte <duarte.denis11@gmail.com>
2020-08-04 20:35:37 -0400

    Reading complete.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,13 +1,39 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@RequestMapping(value="event")
+@RequestMapping(value="events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
 
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
+        return "events/allEvents";
+}
 
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+    }
 
 }
--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("title", "Coding Events");
         return "index";
     }
 }
